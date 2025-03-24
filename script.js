67a071637e6bbf1fc03c5d367765d6418684524d
PrinceKumarCodes <princekumarcbcs2022@gmail.com>
2025-03-24 21:43:28 +0530

    Update files

---
+++ script.js
@@ -0,0 +1,102 @@
+confetti_effect();
+bottom_confetti_effect();
+
+const passwordbox = document.getElementById("password");
+const length = 12;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]></-=";
+
+const allchars = uppercase + lowercase + number + symbol;
+
+function createpassword() {
+  let password = "";
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allchars[Math.floor(Math.random() * allchars.length)];
+  }
+  passwordbox.value = password;
+}
+
+function copypassword() {
+  passwordbox.select();
+  document.execCommand("copy");
+}
+
+//Add confetti_effect
+function confetti_effect() {
+  var end = Date.now() + 7 * 1000; // Run for 7 seconds
+  var colors = [
+    "#00BFFF",
+    "#39FF14",
+    "#FF69B4",
+    "#FF4500",
+    "#32CD32",
+    "#DC143C",
+  ];
+
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors,
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+//Add bottom confetti efect
+function bottom_confetti_effect() {
+  var count = 500;
+  var defaults = {
+    origin: { y: 1.2 },
+  };
+
+  function fire(particleRatio, opts) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio),
+    });
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
